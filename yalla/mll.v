98521747ea23d48c91528b14c66695fac14adff8
Rémi Di Guardia <remi.diguardia@gmail.com>
2021-02-08 10:50:32 +0100

    plan + début couche 1 des proofnets

--- yalla/mll.v
+++ yalla/mll.v
@@ -2,10 +2,15 @@
 
 From Coq Require Import Bool Wf_nat Lia.
 From OLlibs Require Import dectype funtheory List_more List_Type Permutation_Type_more.
+From GraphTheory Require Import mgraph.
 
 Import EqNotations.
 
+Unset Strict Implicit.
+Unset Printing Implicit Defensive.
+Set Bullet Behavior "Strict Subproofs".
 Set Implicit Arguments.
+
 
 (** * MLL formulas *)
 
@@ -96,16 +101,22 @@ Inductive ll : list formula -> Type :=
 | ax_r : forall X, ll (covar X :: var X :: nil)
 | ex_r : forall l1 l2, ll l1 -> Permutation_Type l1 l2 -> ll l2
 | tens_r : forall A B l1 l2, ll (A :: l1) -> ll (B :: l2) -> ll (tens A B :: l2 ++ l1)
-| parr_r : forall A B l, ll (A :: B :: l) -> ll (parr A B :: l).
+| parr_r : forall A B l, ll (A :: B :: l) -> ll (parr A B :: l)
+| cut_r : forall A l1 l2 l3 l4, ll (l1 ++ A :: l2) ->
+             ll (l3 ++ dual A :: l4) -> ll (l3 ++ l2 ++ l1 ++ l4).
 Notation "⊢ l" := (ll l) (at level 70).
+
+(* Bizarrement les arguments implicitent ne fonctionnent plus .... *)
+Arguments ex_r {l1} {l2} pi0 sigma. (* sans effet, demande toujours 4 arguments *)
 
 (** ** Size of proofs *)
 Fixpoint psize l (pi : ll l) :=
 match pi with
 | ax_r _ => 1
-| ex_r pi0 _ => S (psize pi0)
-| tens_r pi1 pi2 => S (psize pi1 + psize pi2)
-| parr_r pi0 => S (psize pi0)
+| ex_r _ _ pi0 _ => S (psize pi0)
+| tens_r _ _ _ _ pi1 pi2 => S (psize pi1 + psize pi2)
+| parr_r _ _ _ pi0 => S (psize pi0)
+| cut_r _ _ _ _ _ pi0 pi1 => S (psize pi0 + psize pi1)
 end.
 
 Lemma psize_pos l (pi : ll l) : 0 < psize pi.
@@ -114,6 +125,225 @@ Proof. destruct pi; cbn; lia. Qed.
 Lemma psize_rew l l' (pi : ll l) (Heq : l = l') : psize (rew Heq in pi) = psize pi.
 Proof. now subst. Qed.
 
+
+(* TODO from here on *)
+From mathcomp Require Import all_ssreflect. (* produces errors if done before TODO solve it*)
+
+(** * Level 0: Multigraphs *)
+(* From the library GraphTheory *)
+Print graph.
+(* inductive def of graphs + common operations -> from library *)
+
+(* Vertex label: rule ; Arrows label: formula *)
+Inductive rule : Type :=
+| ax_l
+| tens_l
+| parr_l
+| cut_l
+| concl_l.
+
+
+(* Equality of [rule] *)
+Definition eqb_rule (A : rule) (B : rule) : bool :=
+match A, B with
+| ax_l, ax_l => true
+| tens_l, tens_l => true
+| parr_l, parr_l => true
+| cut_l, cut_l => true
+| concl_l, concl_l => true
+| _, _ => false
+end.
+
+Lemma eqb_eq_rule : forall A B, eqb_rule A B = true <-> A = B.
+Proof.
+destruct A, B; split; intro H; simpl; simpl in H; trivial; contradict H; easy.
+Qed.
+
+Print DecType.
+About eqb_rule.
+About eqb_form.
+(* ERROR eqb: Not a projection of inductive DecType.
+Definition rules_dectype := {|
+  car := rule;
+  eqb := eqb_rule;
+  eqb_eq := eqb_eq_rule |}.
+*)
+
+(** * Level 1: Multigraphs with some more data *)
+
+(*  graph_of: multigraph
+    sequent: conclusion of the proof
+    order: to each formula of sequent, associate the unique corresponding concl node
+    left (resp. right) gives the corresponding premisse of the node *)
+Set Primitive Projections.
+Record graph_data : Type :=
+  Graph_data {
+      graph_of:> graph rule formula;
+      sequent : list formula;
+      order: 'I_(size sequent) -> vertex graph_of; (* [set x | eqb_rule (vlabel x) concl_l] *)
+      left: vertex graph_of -> edge graph_of;
+      right: vertex graph_of -> edge graph_of }.
+Unset Primitive Projections.
+(*TODO make order into nodes concl, left and right from nodes parr+tens *)
+
+
+Definition graph_ax (x : atom) : graph rule formula :=
+  add_edge (
+    add_vertex (
+      add_edge (
+        add_vertex (
+          unit_graph ax_l)
+        concl_l)
+      (inl tt) (inr tt) (covar x))
+    concl_l)
+  (inl (inl tt)) (inr tt) (var x).
+
+Definition sequent_ax (x : atom) : list formula := covar x :: var x :: nil.
+
+Check Ordinal. Check 'I_2.
+Lemma o : 0<2.
+easy. Qed.
+Lemma oo : 1<2.
+easy. Qed.
+Definition order_ax (x : atom) : 'I_2 -> vertex (graph_ax x) :=
+fun n => match n with
+| Ordinal 0 o => inl (inr tt)
+| Ordinal 1 oo => inr tt
+| _ => inr tt end.
+(* ugly, must be improved *)
+
+Definition left_ax (x : atom) : vertex (graph_ax x) -> edge (graph_ax x) := fun _ => None.
+Definition right_ax (x : atom) : vertex (graph_ax x) -> edge (graph_ax x) := fun _ => None.
+
+Definition graph_data_ax (x : atom) : graph_data := Graph_data (graph_of := graph_ax x) (sequent := sequent_ax x) (order_ax x) (left_ax (x := x)) (right_ax (x := x)).
+(* ugly too *)
+
+
+
+(** * Level 2: Proof Structure *)
+(* def of proof_structure: label on node implies [conditions on arrows] *)
+(* def: record with graph_data + proof of properness *)
+Section Propertness.
+Variables (G : graph_data).
+Implicit Types (x : G).
+Definition edges_out_at x := [set e | source e == x].
+Definition edges_in_at x := [set e | target e == x].
+
+
+Definition proper_concl :=
+  forall x, (vlabel x = concl_l) -> (#|edges_out_at x| == 0) /\ (#|edges_in_at x| == 1)
+.
+
+Definition proper := proper_concl.
+End Propertness.
+
+Lemma proper_ax : forall (x : atom), proper (graph_data_ax x).
+Proof.
+intro; unfold proper.
+unfold proper_concl.
+intros y Hlabel. split.
+- unfold edges_out_at. rewrite cards_eq0.
+  assert (H: forall e, source e <> y).
+      case. case. case. case. easy. easy. simpl.
+      case. intro H. rewrite <-H in Hlabel. simpl in Hlabel. contradict Hlabel. easy.
+      intro e. case. intro H. rewrite <-H in Hlabel. simpl in Hlabel. contradict Hlabel. easy.
+(* euh ça devrait pas du tout marcher ... *)
+      case. intro H. rewrite <-H in Hlabel. simpl in Hlabel. contradict Hlabel. easy.
+Search (_==set0).
+  admit.
+- Search (#|_|==1).
+(*  assert (forall (e : edges_in_at y), e = Some (inl tt)).
+Print graph_ax. *)
+Admitted.
+
+
+(** * Proof Structure of a Proof Sequent *)
+(* from sequent proof to proof structure:
+Fixpoint ps l (pi : ll l) : graph :=
+match pi with
+| ax_r X => graph with a node labelled ax, two nodes c_1 and c_2, 
+    an edge ax->c_1 labelled covar X, another ax->c_2 labelled var X
+
+| ex_r pi0 sigma => take ps (pi_0), re-label the c_i into c_sigma(i):
+  turn the perm on list of formulas into one on list of integers, should be easy?
+About Permutation_Type. -> make the list of [c_i], apply perm on it, then re-label nodes c
+
+| tens_r pi0 pi1 => take G0=ps (pi0) and G1=ps (pi1)
+    In G0: turn c_i into c_n+i-1 for i\neq0, for c_0 turn into c_inf, with n =#conc in G1
+    make a disjoint union of G0 and G1
+    find edges on c_0 and c_n, and their endpoints (char. proof str. -> unicity)
+    remove edges ?1->c_0 (B), ?0->c_inf(A)
+    remove nodes c0 c_inf
+    add nodes t(tens) c0
+    add edges ?0->t (A,0), ?1->t (B,1), t->c_0 (tens A B)
+
+| parr_r pi0 => take G0=ps (pi0)
+    find edges on c_0 and c_1, and their endpoints (char. proof str. -> unicity)
+    remove edges ?0->c_0 (A), ?1->c_1(B)
+    remove nodes c0 c_1
+    re-label c_i into c_i-1
+    add nodes p(parr) c0
+    add edges ?0->p (A,0), ?1->p (B,1), p->c_0 (parr A B)
+
+| cut_r A l1 l2 l3 l4 pi0 pi1 => take 0=ps (pi0) and G1=ps (pi1), ni=#li for all i
+    In G0: turn c_i into c_n3+n2+i for i<n1, for c_n1 turn into c_inf1, c_n3+(i-n1) otherwise
+    In G1: for c_n3 turn into c_inf2,  c_n3+n2+n1+(i-n3) for i>n3
+    make a disjoint union of G0 and G1
+    find edges on c_inf0 and c_inf1, and their endpoints (char. proof str. -> unicity)
+    remove edges ?0->c_inf0 (var A), ?1->c_inf1(covar A)
+    remove nodes cinf0 c_inf1
+    add nodes c(cut)
+    add edges ?0->c (var A,0), ?1->c (covar A,1)
+*)
+
+(*
++inductive proof that ps(pi) is a proof_structure
++inductive proof that order list of l (pi : ll l) corresponds to order c_i of the graph
+*)
+
+
+(** * Correctness Criteria: Danos-Regnier *)
+(* Switching Graph *)
+(* SG (PS):
+for a proof structure PS, get P the nodes labelled parr, then a s.g. is given by:
+phi: P -> B, G_phi = G where on node v\in P, arrow ?->v(A,phi(v)) is deleted, add node c_v, edge ?->c_v(A)
+    then remove direction /!/ depends on the trees in the library
+*)
+
+(* Criteria: acyclic and connected *)
+(* need def for acyclic + connected, or just for tree ?-> considering tree may change the proofs ?! *)
+
+(*
+Definition is_correct PS :=
+  forall phi, acyclic SG (PS) (phi) /\ connected SG (PS) (phi).
+or with is_tree (already in the lib) ???
+  forall phi, is_tree SG (PS) (phi).
+*)
+
+(* Soundness of correctness *)
+(*
+Lemma sound l (pi : ll l) : is_correct ps (pi).
+*)
+
+(** * Cut Elimination *)
+(*
+Inductive red : 
+| ax->cut: merge the 2 nodes, then merge the final node with the node above, keep label of above
+| tens->cut<-parr: merge the 3 nodes into the cut one, then split it with the good edges, give label cut to both
+=> need procedure merge, where a parent node absorb another, keeping its own label (may be used before instead of removing edge then adding edge ???)
+*)
+(* lemma: if R is correct and R reduces to R', then R' is correct *)
+(* lemma: applying red while we can yields a cut-free ps:
+    there is a cut node => one of the two subgraphs (*2 by symmetry) => reduction to another graph *)
+(* lemma: sub-confluence + convergence *)
+
+(** * Sequentialization *)
+(* many things to do: spliting tens / cut, blocking parr, always a terminal parr or a splitting *)
+(* function to turn a ps into a sequential proof *)
+
+
+
+(** * PREVIOUS CONTENT of the file mll.v *)
 
 (** * Cut Elimination *)
 
@@ -321,5 +551,6 @@ apply (sym_induction_ll (fun A B l1 l2 l3 l4 pi1 pi2 => B = dual A -> ll (l3 ++ 
   rewrite <- app_assoc; apply IHC; auto.
   now rewrite <- app_nil_l; apply IHD.
 Qed.
+*)
 
 End Atoms.
